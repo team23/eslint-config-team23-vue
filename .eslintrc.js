bcca10fa5bef2d97fd1947dd4070f3044505d8e8
Laura Schmid <schmid@team23.de>
2023-08-24 08:29:44 +0200

    feat: update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,5 +43,18 @@ module.exports = {
                 'prefer-arrow/prefer-arrow-functions': 'off',
             },
         },
+        {
+            files: ['*.ts', '*.tsx', '*.vue'],
+            rules: {
+                // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
+                // does not work with type definitions
+                'no-unused-vars': 'off',
+                // TS already checks for that, and Typescript-Eslint recommends to disable it
+                // eslint-disable-next-line max-len
+                // https://typescript-eslint.io/linting/troubleshooting#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+                'no-undef': 'off',
+                '@typescript-eslint/no-unused-vars': 'warn',
+            },
+        },
     ],
 };
