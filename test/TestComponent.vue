6b9623d940b5a7d035d3a3ccae00d92f12f822a0
Erik Herberg <herberg@team23.de>
2024-02-06 13:34:52 +0100

    fix: linting errors

--- test/TestComponent.vue
+++ test/TestComponent.vue
@@ -1,3 +1,17 @@
+<script lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+</script>
+
 <template>
     <div class="counter">
         <h2>Counter: {{ count }}</h2>
@@ -13,30 +27,6 @@
         </button>
     </div>
 </template>
-
-<script>
-import { ref } from 'vue';
-
-export default {
-  setup() {
-    const count = ref(0);
-
-    const increment = () => {
-      count.value++;
-    };
-
-    const decrement = () => {
-      count.value--;
-    };
-
-    return {
-      count,
-      increment,
-      decrement,
-    };
-  },
-};
-</script>
 
 <style scoped>
 .counter {
